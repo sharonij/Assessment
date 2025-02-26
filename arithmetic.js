8fe2d14d58e46a1ac159253654ac8c22f3b176b3
Sharonij <sharonagwah@gmail.com>
2025-02-26 19:10:34 +0100

    Sharon

---
+++ arithmetic.js
@@ -0,0 +1,18 @@
+// Declare two numbers
+let num1 = 15;
+let num2 = 4;
+
+// Perform arithmetic operations
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+let remainder = num1 % num2;
+
+// Print results
+console.log(`Given numbers: ${num1} and ${num2}`);
+console.log(`Addition: ${num1} + ${num2} = ${sum}`);
+console.log(`Subtraction: ${num1} - ${num2} = ${difference}`);
+console.log(`Multiplication: ${num1} * ${num2} = ${product}`);
+console.log(`Division: ${num1} / ${num2} = ${quotient}`);
+console.log(`Modulus: ${num1} % ${num2} = ${remainder}`);
